8efaab4a680d0344ed2c2e3f857737bfdd89f8be
mkhondsnow <129342008+mkhondsnow@users.noreply.github.com>
2023-03-29 18:54:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bliueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Soothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
